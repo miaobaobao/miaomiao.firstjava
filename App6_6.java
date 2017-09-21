4525e1ded360a313fc3d4b6665350520fcc65c37
catbaby <31614022+miaobaobao@users.noreply.github.com>
2017-09-21 21:34:54 +0800

    矩阵转置的输出

--- App6_6.java
+++ App6_6.java
@@ -11,7 +11,7 @@ public class App6_6 {
 		b = trans.transpose(a);//把数组a作为参数调用，返回值赋给b
 		for(int i=0;i<b.length;i++) {  //输出数组
 			for(int j=0;j<b[i].length;j++) {
-				System.out.println(b[i][j] + " ");
+				System.out.print(b[i][j] + " ");
 			}
 			System.out.println("\n");
 		}
@@ -24,7 +24,7 @@ class Trans {
 	int temp;
 	int [][] transpose(int [][]array){
 		for(int i=0;i<array.length;i++) {
-			for(int j=0;j<=array[i].length;j++) {
+			for(int j=0;j<array[i].length;j++) {
 				temp = array [i][j];    //对角线相转换
 				array[i][j] = array[j][i];
 				array[j][i] = temp;
